aa6af6a6d64713a4f9629b18749d22647a9057ff
Robert Novotny <novotnr0@gmail.com>
2017-12-12 12:53:19 +0100

    Implementovane tela aktorov

--- src/main/java/sk/eastcode/freq/DocumentFrequencyCounter.java
+++ src/main/java/sk/eastcode/freq/DocumentFrequencyCounter.java
@@ -3,10 +3,17 @@ package sk.eastcode.freq;
 import akka.actor.AbstractLoggingActor;
 import akka.actor.Props;
 
+import java.util.Map;
+
 public class DocumentFrequencyCounter extends AbstractLoggingActor {
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder().match(String.class, this::handleDocument).build();
+    }
+
+    void handleDocument(String document) {
+        Map<String, Long> frequencies = Utils.calculateFrequencies(document);
+        getSender().tell(frequencies, getSelf());
     }
 
     public static Props props() {
--- src/main/java/sk/eastcode/freq/Master.java
+++ src/main/java/sk/eastcode/freq/Master.java
@@ -3,10 +3,23 @@ package sk.eastcode.freq;
 import akka.actor.AbstractLoggingActor;
 import akka.actor.Props;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Master extends AbstractLoggingActor {
+    private Map<String, Long> allFrequencies = new HashMap<>();
+
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder()
+                .match(Map.class, this::handleFrequencies)
+                .build();
+    }
+
+    void handleFrequencies(Map<String, Long> frequencies) {
+        this.allFrequencies = Utils.aggregate(frequencies, allFrequencies);
+
+        log().info("Global frequencies: {}", this.allFrequencies);
     }
 
     public static Props props() {
